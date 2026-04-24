19924996d3d6bb4931d1a37517d9da3717130867
Dmytro Kovalenko <dmytrokovalenko.new@gmail.com>
2026-04-23 22:04:14 -0600

    feat(Icons): update icon strokes to use currentColor for better theming support

--- laravel/resources/js/icons/IconBasement.tsx
+++ laravel/resources/js/icons/IconBasement.tsx
@@ -3,17 +3,17 @@ import type { SVGProps } from "react";
 export default function IconBasement(props: SVGProps<SVGSVGElement>){
     return (
         <svg {...props} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-            <path d="M15 1.66663H5.00001C4.07954 1.66663 3.33334 2.41282 3.33334 3.33329V16.6666C3.33334 17.5871 4.07954 18.3333 5.00001 18.3333H15C15.9205 18.3333 16.6667 17.5871 16.6667 16.6666V3.33329C16.6667 2.41282 15.9205 1.66663 15 1.66663Z" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M7.5 18.3333V15H12.5V18.3333" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M6.66666 5H6.67499" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M13.3333 5H13.3417" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M10 5H10.0083" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M10 8.33337H10.0083" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M10 11.6666H10.0083" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M13.3333 8.33337H13.3417" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M13.3333 11.6666H13.3417" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M6.66666 8.33337H6.67499" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M6.66666 11.6666H6.67499" stroke="#fff" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M15 1.66663H5.00001C4.07954 1.66663 3.33334 2.41282 3.33334 3.33329V16.6666C3.33334 17.5871 4.07954 18.3333 5.00001 18.3333H15C15.9205 18.3333 16.6667 17.5871 16.6667 16.6666V3.33329C16.6667 2.41282 15.9205 1.66663 15 1.66663Z" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M7.5 18.3333V15H12.5V18.3333" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M6.66666 5H6.67499" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13.3333 5H13.3417" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M10 5H10.0083" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M10 8.33337H10.0083" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M10 11.6666H10.0083" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13.3333 8.33337H13.3417" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13.3333 11.6666H13.3417" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M6.66666 8.33337H6.67499" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M6.66666 11.6666H6.67499" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
 }
--- laravel/resources/js/icons/IconTrendingUp.tsx
+++ laravel/resources/js/icons/IconTrendingUp.tsx
@@ -4,8 +4,8 @@ import type { SVGProps } from "react";
 export default function IconTrendingUp(props: SVGProps<SVGSVGElement>){
     return (
         <svg {...props} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512">
-            <path fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M352 144h112v112" />
-            <path fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="m48 368l121.37-121.37a32 32 0 0 1 45.26 0l50.74 50.74a32 32 0 0 0 45.26 0L448 160" />
+            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M352 144h112v112" />
+            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="m48 368l121.37-121.37a32 32 0 0 1 45.26 0l50.74 50.74a32 32 0 0 0 45.26 0L448 160" />
         </svg>
     );
 }
